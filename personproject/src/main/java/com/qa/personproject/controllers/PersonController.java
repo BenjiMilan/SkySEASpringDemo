d90e303a599718e7c0957db24d21328b670f602a
Benji Milan <benjamin.milan@sky.uk>
2022-09-28 20:36:45 +0100

    Added swagger2 functionality

--- personproject/src/main/java/com/qa/personproject/controllers/PersonController.java
+++ personproject/src/main/java/com/qa/personproject/controllers/PersonController.java
@@ -2,9 +2,12 @@ package com.qa.personproject.controllers;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
+import javax.validation.constraints.Size;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+
 
 import com.qa.personproject.entities.Person;
 import com.qa.personproject.services.PersonService;
@@ -13,31 +16,27 @@ import com.qa.personproject.services.PersonService;
 @RequestMapping("people")
 public class PersonController {
 
+    @Autowired
     private PersonService service;
 
-    public PersonController(PersonService service) {
-        super();
-        this.service = service;
-    }
-
-    @PostMapping("new/{id}")
-    public void createPerson(@PathParam("firstName") String firstName, @PathParam("lastName") String lastName, @PathParam("age") int age) {
+    @PostMapping("new")
+    public void createPerson(@RequestParam @Size(min = 2, max = 30) String firstName, @RequestParam @Size(min = 2, max = 30) String lastName, @RequestParam int age) {
         service.newPerson(firstName, lastName, age);
     }
 
     @ResponseBody
-    @GetMapping(value = "/all", produces = "application/json")
+    @GetMapping(value = "all", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> getPeople() {
         return service.getPeople();
     }
 
     @DeleteMapping("delete/{id}")
-    public void deletePerson(@PathParam("id") long id) {
+    public void deletePerson(@RequestParam long id) {
         service.deletePerson(id);
     }
 
     @PutMapping("update/{id}")
-    public void updatePerson(@PathParam("id") long id, @PathParam("firstName") String firstName, @PathParam("lastName") String lastName, @PathParam("age") int age) {
+    public void updatePerson(@RequestParam long id, @RequestParam @Size(min = 2, max = 30) String firstName, @RequestParam @Size(min = 2, max = 30) String lastName, @RequestParam int age) {
         service.updatePerson(id, firstName, lastName, age);
     }
 
--- personproject/src/main/java/com/qa/personproject/entities/PersonRepository.java
+++ personproject/src/main/java/com/qa/personproject/entities/PersonRepository.java
@@ -1,9 +1,24 @@
 package com.qa.personproject.entities;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+//import java.util.List;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
     
+    @Query("SELECT p from Person p")
+    Optional<Person> findAllJPQL();
+
+    Optional<Person> getPersonByFirstNameIgnoreCase(String firstName);
+
+    Optional<Person> getPersonById(long id);
+
+    Optional<Person> findPersonByFirstNameAndAge(String firstName, long id);
+
+    Optional<Person> findPersonByAgeLessThan(int age);
 }
